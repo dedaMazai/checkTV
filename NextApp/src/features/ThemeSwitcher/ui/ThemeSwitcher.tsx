1883a8fdbdfb8686d9a58ccd0cd84b99ec983606
Andrei Chipizubov <andryukha.chipizybov@mail.ru>
2023-11-17 11:21:50 +0300

    fix

--- NextApp/src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ NextApp/src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import { classNames } from "@/shared/lib/classNames/classNames";
 import { Button } from "@/shared/ui/Button";
 import { useTranslation } from "next-i18next";
@@ -11,19 +12,54 @@ interface ThemeSwitcherProps {
 export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
   const { t } = useTranslation("common");
   const { theme, setTheme } = useTheme();
+  const refButton = useRef<HTMLButtonElement>(null);
+
+  const [isMouseOnButton, setIsMouseOnButton] = useState(false);
 
   const onToggleHandler = useCallback(() => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
+    setTheme(theme === "light" ? "dark" : "light");
   }, [setTheme, theme]);
 
+  const onKeyDown = useCallback((e: KeyboardEvent) => {
+    if (e.key === "Enter" && isMouseOnButton) {
+      refButton.current?.click();
+    }
+  }, [isMouseOnButton]);
+
+  useEffect(() => {
+    document.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", onKeyDown);
+    };
+  }, [onKeyDown]);
+
+  useEffect(() => {
+    refButton.current?.addEventListener('mouseenter', () => {setIsMouseOnButton(true)});
+    refButton.current?.addEventListener("mouseleave", () => {setIsMouseOnButton(false)});
+
+    return () => {
+      refButton.current?.removeEventListener("mouseenter", () => {setIsMouseOnButton(true)});
+      refButton.current?.removeEventListener("mouseleave", () => {setIsMouseOnButton(false)});
+    };
+  }, [onKeyDown]);
+
+  useEffect(() => {
+    console.log(isMouseOnButton)
+  }, [isMouseOnButton]);
+
   return (
-    <Button
-      variant="clearGrey"
-      className={classNames(className)}
-      onClick={onToggleHandler}
-      fullHeight
-    >
+    // <input  type='button' onKeyDown={(e) => something(e) } value="11111S" />
+    // <Button
+    //   variant="clearGrey"
+    //   className={classNames(className)}
+    //   onClick={onToggleHandler}
+    //   fullHeight
+    // >
+    //   {t("qqq")}
+    // </Button>
+    <button ref={refButton} onClick={onToggleHandler}>
       {t("qqq")}
-    </Button>
+    </button>
   );
 };
