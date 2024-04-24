37e9710e82fe018d84650dedecac13a0c16deb8e
Andrei Chipizubov <andryukha.chipizybov@mail.ru>
2024-04-24 14:18:28 +0300

    fix

--- NextApp/pages/index.ts
+++ NextApp/pages/index.ts
@@ -9,21 +9,21 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 //   },
 // });
 
-export async function getServerSideProps(context: any) {
-  const userAgent = context.req.headers['user-agent'] || '';
+// export async function getServerSideProps(context: any) {
+//   const userAgent = context.req.headers['user-agent'] || '';
 
-  const response = await fetch('https://fakerapi.it/api/v1/books?_quantity=2', {
-    method: 'GET',
-  })
+//   const response = await fetch('https://fakerapi.it/api/v1/books?_quantity=2', {
+//     method: 'GET',
+//   })
 
-  const res = await response.json();
+//   const res = await response.json();
 
-  return {
-    props: {
-      ...(await serverSideTranslations(context.locale ?? "ru", ["common"])),
-      user: res.data,
-    },
-  };
-}
+//   return {
+//     props: {
+//       ...(await serverSideTranslations(context.locale ?? "ru", ["common"])),
+//       user: res.data,
+//     },
+//   };
+// }
 
 export default MainPage;
